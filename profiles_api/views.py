4d78d394f6ba8139e5059a52201c158d743611df
Burak CAN <drcan94@gmail.com>
2021-12-11 16:39:55 +0300

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
-from .serializers import HelloSerializer
+from .serializers import HelloSerializer, UserProfileSerializer
+from .models import UserProfile
+from .permissions import UpdateOwnProfile
 
 class HelloApiView(APIView):
     """ Test APIView """
@@ -94,3 +97,13 @@ class HelloViewSet(ViewSet):
     def destroy(self, request, pk=None):
         """ Delete an Object """
         return Response({'http_method' : 'DELETE'})
+
+
+class UserProfileViewSet(ModelViewSet):
+    """ Handle creating and updating profiles """
+
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
